b4ac20a30f81bcc0091c7209630ac301ea8dde1b
Rodrigodip <rodrigodip@gmail.com>
2026-06-22 19:36:34 -0300

    first commit

---
+++ src/main/java/com/rodrigodip/workshop_springboot4_jpa/WorkshopSpringboot4JpaApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigodip.workshop_springboot4_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringboot4JpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringboot4JpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodrigodip/workshop_springboot4_jpa/WorkshopSpringboot4JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigodip.workshop_springboot4_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringboot4JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
