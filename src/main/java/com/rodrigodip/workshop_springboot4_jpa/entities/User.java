cc5d26adb023976437f06a97bf1ea7744fd0eb28
Rodrigodip <rodrigodip@gmail.com>
2026-06-23 20:02:52 -0300

    feat: creates a test profile, configure and tests H2 embeded DB using JPA

--- src/main/java/com/rodrigodip/workshop_springboot4_jpa/entities/User.java
+++ src/main/java/com/rodrigodip/workshop_springboot4_jpa/entities/User.java
@@ -2,8 +2,18 @@ package com.rodrigodip.workshop_springboot4_jpa.entities;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "td_user")
 public class User implements Serializable {
 
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
         private Long id;
         private String name;
         private String email;
