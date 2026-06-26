75c3d7bdcc07a8be0c4707069aad47264f19ac42
Rodrigodip <rodrigodip@gmail.com>
2026-06-26 18:30:25 -0300

    feat: create Order entity with ManyToOne relation to User

--- NOTES.md
+++ NOTES.md
@@ -5,3 +5,6 @@ __Java Persistence API__ **now** __Jakarta Persistence__
 
 JPA is a standard Java specification that simplifies how Java objects (data) are saved or "persisted" into relational databases.
 It's a group of rules, annotations and interfaces that maps Java objects and standardizes persistence operations.
+
+
+### Instant Java Class
--- src/main/java/com/rodrigodip/workshop_springboot4_jpa/entities/Order.java
+++ src/main/java/com/rodrigodip/workshop_springboot4_jpa/entities/Order.java
@@ -1,40 +1,36 @@
 package com.rodrigodip.workshop_springboot4_jpa.entities;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.Instant;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "td_user")
-public class User implements Serializable {
+@Table(name = "tb_order")
+public class Order implements Serializable {
 
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         private Long id;
-        private String name;
-        private String email;
-        private String phone;
-        private String password;
+        private Instant moment;
 
-        @OneToMany(mappedBy = "client")
-        private List<Order> orders = new ArrayList<>();
+        @ManyToOne
+        @JoinColumn(name = "client_id")
+        private User client;
 
-        public User() {
+        public Order() {
         }
 
-        public User(Long id, String name, String email, String phone, String password) {
+        public Order(Long id, Instant moment, User client) {
                 this.id = id;
-                this.name = name;
-                this.email = email;
-                this.phone = phone;
-                this.password = password;
+                this.moment = moment;
+                this.client = client;
         }
 
         public Long getId() {
@@ -45,40 +41,20 @@ public class User implements Serializable {
                 this.id = id;
         }
 
-        public String getName() {
-                return name;
+        public Instant getMoment() {
+                return moment;
         }
 
-        public void setName(String name) {
-                this.name = name;
+        public void setMoment(Instant moment) {
+                this.moment = moment;
         }
 
-        public String getEmail() {
-                return email;
+        public User getClient() {
+                return client;
         }
 
-        public void setEmail(String email) {
-                this.email = email;
-        }
-
-        public String getPhone() {
-                return phone;
-        }
-
-        public void setPhone(String phone) {
-                this.phone = phone;
-        }
-
-        public String getPassword() {
-                return password;
-        }
-
-        public void setPassword(String password) {
-                this.password = password;
-        }
-
-        public List<Order> getOrders() {
-                return orders;
+        public void setClient(User client) {
+                this.client = client;
         }
 
         @Override
@@ -97,7 +73,7 @@ public class User implements Serializable {
                         return false;
                 if (getClass() != obj.getClass())
                         return false;
-                User other = (User) obj;
+                Order other = (Order) obj;
                 if (id == null) {
                         if (other.id != null)
                                 return false;
