9aba98abcd7fb8c0d4838b1dc4182430cf8e86b1
Rodrigodip <rodrigodip@gmail.com>
2026-06-30 10:06:27 -0300

    feat: create OrderStatus enum with payment states

--- src/main/java/com/rodrigodip/workshop_springboot4_jpa/config/TestConfig.java
+++ src/main/java/com/rodrigodip/workshop_springboot4_jpa/config/TestConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.rodrigodip.workshop_springboot4_jpa.entities.Order;
 import com.rodrigodip.workshop_springboot4_jpa.entities.User;
+import com.rodrigodip.workshop_springboot4_jpa.entities.enums.OrderStatus;
 import com.rodrigodip.workshop_springboot4_jpa.repositories.OrderRepository;
 import com.rodrigodip.workshop_springboot4_jpa.repositories.UserRepository;
 
@@ -31,9 +32,9 @@ public class TestConfig implements CommandLineRunner {
                 User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
                 User u3 = new User(null, "Rodrigo", "rod@gmail.com", "992614515", "098787");
 
-                Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-                Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-                Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+                Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+                Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+                Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
                 userRepository.saveAll(Arrays.asList(u1, u2, u3));
                 orderRepository.saveAll(Arrays.asList(o1, o2, o3));
