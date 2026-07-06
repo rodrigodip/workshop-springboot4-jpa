921f9e7c85a753a09f3a1b27691d2239b67be73f
Rodrigo <rodrigodip@gmail.com>
2026-07-06 19:59:41 -0300

    fix: correct OrderItemRepository signature to JpaRepository<OrderItem, OrderItemPK>

--- src/main/java/com/rodrigodip/workshop_springboot4_jpa/repositories/OrderItemRepository.java
+++ src/main/java/com/rodrigodip/workshop_springboot4_jpa/repositories/OrderItemRepository.java
@@ -3,7 +3,8 @@ package com.rodrigodip.workshop_springboot4_jpa.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.rodrigodip.workshop_springboot4_jpa.entities.OrderItem;
+import com.rodrigodip.workshop_springboot4_jpa.entities.pk.OrderItemPK;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> {
         // No need for implementations Spring Data JPA takes care of it.
 }
