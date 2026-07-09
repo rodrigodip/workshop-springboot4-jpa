57f0c70c00b20cc3b7d20e2511e0ca1287060072
Rodrigo <rodrigodip@gmail.com>
2026-07-09 19:22:46 -0300

    feat: implement User insert endpoint with service and controller
    - Add POST /users endpoint in UserController
    - Implement save() method in UserService
    - Enable user creation with request body

--- src/main/java/com/rodrigodip/workshop_springboot4_jpa/controller/UserController.java
+++ src/main/java/com/rodrigodip/workshop_springboot4_jpa/controller/UserController.java
@@ -1,16 +1,20 @@
 package com.rodrigodip.workshop_springboot4_jpa.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rodrigodip.workshop_springboot4_jpa.entities.User;
 import com.rodrigodip.workshop_springboot4_jpa.services.UserService;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -32,6 +36,18 @@ public class UserController {
         public ResponseEntity<User> findById(@PathVariable Long id) {
                 User userFound = userService.FindByID(id);
                 return ResponseEntity.ok().body(userFound);
+        }
+
+        @PostMapping
+        public ResponseEntity<User> insert(@RequestBody User newUser) {
+                newUser = userService.insert(newUser);
+                URI uri = ServletUriComponentsBuilder
+                                .fromCurrentRequest()
+                                .path("/{id}")
+                                .buildAndExpand(newUser.getId())
+                                .toUri();
+
+                return ResponseEntity.created(uri).body(newUser);
         }
 
 }
--- src/main/java/com/rodrigodip/workshop_springboot4_jpa/services/UserService.java
+++ src/main/java/com/rodrigodip/workshop_springboot4_jpa/services/UserService.java
@@ -27,4 +27,8 @@ public class UserService {
                                 .orElseThrow(
                                                 () -> new RuntimeException("User Not Found."));
         }
+
+        public User insert(User user) {
+                return userRepository.save(user);
+        }
 }
