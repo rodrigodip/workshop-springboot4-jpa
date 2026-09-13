baead70305077b72eda70696ee05f67cb4441331
Rodrigodip <rodrigodip@gmail.com>
2026-09-13 15:08:03 -0300

    fix: correct timestamp pattern from HH-mm-ss to HH:mm:ss
    - Fix date format in Order and StandardError
    - Use proper colon separator in time portion
    - Affects JSON output for date fields

--- src/main/java/com/rodrigodip/workshop_springboot4_jpa/controller/exceptions/StandardError.java
+++ src/main/java/com/rodrigodip/workshop_springboot4_jpa/controller/exceptions/StandardError.java
@@ -7,7 +7,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 public class StandardError implements Serializable {
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH-mm-ss'Z'", timezone = "GMT")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant timestamp;
     private Integer status;
     private String error;
